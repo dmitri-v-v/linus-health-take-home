c8c7e8bd9bbc41f313722b1d67f4a6b20a606ee0
Dmitri Vassiliev <dmitri.vassiliev@pm.me>
2024-04-20 21:52:16 -0400

    Improve table schemas for better performnce and flexibility by adding id columns.

--- src/db/migrations/20240420114005_create_provider_table.ts
+++ src/db/migrations/20240420114005_create_provider_table.ts
@@ -5,10 +5,11 @@ export async function up(knex: Knex): Promise<void> {
 	const tableExists = await knex.schema.hasTable('provider');
 
 	if (!tableExists) {
-		console.log('Creating provider table');
+		console.log('Creating Provider table');
 
 		return knex.schema.createTable('provider', table => {
-			table.string('npi').primary();
+			table.increments('id');
+			table.string('npi').unique().index();
 			table.string('first_name').notNullable();
 			table.string('last_name').notNullable();
 		});
--- src/db/migrations/20240421003450_create_patient_table.ts
+++ src/db/migrations/20240421003450_create_patient_table.ts
@@ -5,16 +5,17 @@ export async function up(knex: Knex): Promise<void> {
 	const tableExists = await knex.schema.hasTable('patient');
 
 	if (!tableExists) {
-		console.log('Creating patient table');
+		console.log('Creating Patient table');
 
 		return knex.schema.createTable('patient', table => {
-			table.string('mrn').primary();
+			table.increments('id');
+			table.string('mrn').unique().index();
 			table.string('first_name').notNullable();
 			table.string('last_name').notNullable();
 			table.date('birth_date').notNullable();
 			table.date('appointment_date').notNullable();
 			table.string('location').notNullable();
-			table.string('general_practitioner').references('npi').inTable('provider').notNullable();
+			table.integer('general_practitioner_id').references('id').inTable('provider').notNullable();
 		});
 	}
 }
