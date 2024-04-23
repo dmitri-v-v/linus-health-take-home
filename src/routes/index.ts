e64db76cde03a0ae023a583ed74c0cd0f783a308
Dmitri Vassiliev <dmitri.vassiliev@pm.me>
2024-04-23 10:06:05 -0400

    Add endpoint to get providers by location

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { getAppointmentsByDateRange } from '../services/appointmentService';
+import { AppointmentResponse, getAppointmentsByDateRange } from '../services/appointmentService';
 import { getHealthStatus } from '../services/healthService';
 import { 
 	filterPatients, 
@@ -7,20 +7,21 @@ import {
 	getPatientData, 
 	PatientDetailsResponse 
 } from '../services/patientService';
+import { getProvidersByLocation, ProviderResponse } from '../services/providerService';
 
 
 export const router = Router({});
 
 /**
  * Validates that the provided `params` in the request `req` object are all strings. If any are not, uses the response `res` 
- * object to send a 400 response.
+ * object to send a 400 response. This was mainly added to aid with TypeScript's type enforcement.
  * 
  * @param req The request containing the query params
  * @param res The response that will be sent if any of the params are invalid
  * @param params The list of parameter names to check
  * @returns 
  */
-function validateQueryParams(req: any, res: any, ...params: any[]) {
+function validateQueryParamIsString(req: any, res: any, ...params: any[]) {
 	for (const param of params) {
 		const value = req.query[param];
 
@@ -54,13 +55,7 @@ router.get('/health', async (req, res) => {
  *  * birth date
  *  * Their provider's NPI
  */
-router.get('/patients', async (req, res): Promise<FilterPatientsResponse | Record<string, any>> => {
-	const queryParamsValidationResponse = validateQueryParams(req, res, 'firstName', 'lastName', 'birthDate', 'npi');
-
-	if (queryParamsValidationResponse) {
-		return queryParamsValidationResponse;
-	}
-
+router.get('/patients', async (req, res): Promise<void> => {
 	try {
 		let { firstName, lastName, birthDate, npi } = req.query as {
 			firstName?: string;
@@ -73,35 +68,35 @@ router.get('/patients', async (req, res): Promise<FilterPatientsResponse | Recor
 		const validatedBirthDate = validateDateQueryParameter('birthDate', birthDate, res);
 
 		if (validatedBirthDate && typeof validatedBirthDate !== 'string' ) {
-			return validatedBirthDate; // Means we got the invalid date response.
+			return; // Means we sent the invalid date response.
 		}
 
-		const patients = await filterPatients(firstName, lastName, validatedBirthDate, npi);
+		const patients: FilterPatientsResponse[] = await filterPatients(firstName, lastName, validatedBirthDate, npi);
 
 		if (patients.length === 0) {
-			return res.status(404).json({ message: 'No patients found matching the filter criteria.'});
+			res.status(404).json({ message: 'No patients found matching the filter criteria.'});
+		} else {
+			res.json(patients);
 		}
-		return res.json(patients);
 	} catch (err) {
 		console.error(err);
-		return res.status(500).json({ error: 'An error occurred while retrieving patients' });
+		res.status(500).json({ error: 'An error occurred while retrieving patients' });
 	}
 });
 
 /**
  * The endpoint for retrieving a patient's details by their MRN.
  */
-router.get('/patients/:mrn', async (req, res) => {
+router.get('/patients/:mrn', async (req, res): Promise<void> => {
 	const { mrn } = req.params;
-	let patient: PatientDetailsResponse;
 
 	try {
-		patient = await getPatientData(mrn);
+		const patient: PatientDetailsResponse = await getPatientData(mrn);
 
 		if (patient) {
-			return res.json(patient);
+			res.json(patient);
 		} else {
-			return res.status(404).json({ message: 'No patient found with that mrn.'});
+			res.status(404).json({ message: 'No patient found with that mrn.'});
 		}
 	} catch (err) {
 		console.error(err);
@@ -112,37 +107,63 @@ router.get('/patients/:mrn', async (req, res) => {
 /**
  * The endpoint for retrieving a list of appointments by a date range.
  */
-router.get('/appointments', async (req, res) => {
+router.get('/appointments', async (req, res): Promise<void> => {
     try {
-        const { startDate, endDate } = req.query as {
-			startDate?: string,
-			endDate?: string
-		};
+		const startDate = req.query.startDate as string;
+		const endDate = req.query.endDate as string;
 
 		if (!startDate || !endDate) {
-			return res.status(400).json({error: 'Both startDate and endDate query parameters are required.'});
+			res.status(400).json({error: 'Both startDate and endDate query parameters are required.'});
+			return;
 		}
 
 		const validatedStartDate = validateDateQueryParameter('startDate', startDate, res);
 		const validatedEndDate = validateDateQueryParameter('endDate', endDate, res);
 
-		if (typeof validatedStartDate !== 'string') {
-			return validatedStartDate;
+		if (typeof validatedStartDate !== 'string' || typeof validatedEndDate !== 'string') {
+			return;
 		}
 
-		if (typeof validatedEndDate !== 'string') {
-			return validatedEndDate;
-		}
-
-        const appointments = await getAppointmentsByDateRange(startDate, endDate);
+        const appointments: AppointmentResponse[] = await getAppointmentsByDateRange(startDate, endDate);
 
 		if (appointments.length === 0) {
-			return res.status(404).json({ message: 'No appointments found within that date range.'});
+			res.status(404).json({ message: 'No appointments found within that date range.'});
+		} else {
+			res.json(appointments);
 		}
-        
-		return res.json(appointments);
     } catch (err) {
         console.error(err);
 		res.status(500).json({ error: 'An error occurred while retrieving the appointments.' });
     }
+});
+
+/**
+ * The endpoint for retrieving the health providers for a given location.
+ */
+router.get('/providers', async (req, res): Promise<void> => {
+	if (!req.query.location) {
+		res.status(400).json({ error: 'location is a required query parameter'});
+		return;
+	}
+
+	const queryParamsValidationResponse = validateQueryParamIsString(req, res, 'location');
+
+	if (queryParamsValidationResponse) {
+		return;
+	}
+
+	try {
+		const location = req.query.location as string;
+
+		const providers: ProviderResponse[] = await getProvidersByLocation(location);
+
+		if (providers.length === 0) {
+			res.status(404).json({ message: 'No providers found for that location.'});
+		} else {
+			res.json(providers);
+		}
+	} catch (err) {
+		console.error(err);
+		res.status(500).json({ error: 'An error occurred while retrieving providers' });
+	}
 });
---
+++ src/services/providerService/index.ts
@@ -0,0 +1,26 @@
+import { db } from '../../db';
+import { Provider } from '../../models';
+
+
+export type ProviderResponse = Omit<Provider, 'id'>;
+
+export const getProvidersByLocation = async (location: string): Promise<ProviderResponse[]> => {
+    let providers: ProviderResponse[] = [];
+
+    try {
+        providers = await db<ProviderResponse>('provider')
+            .select(
+                'provider.first_name as firstName',
+                'provider.last_name as lastName',
+                'npi',
+            )
+            .join('patient', 'patient.general_practitioner_id', '=', 'provider.id')
+            .where('patient.location', location)
+            .orderBy('lastName')
+            .orderBy('firstName')
+    } catch (error) {
+        console.error('Error fetching providers', error);
+    }
+
+    return providers;
+};
